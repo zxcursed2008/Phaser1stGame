bed2e2f0918575f700e53d969b8810ea49f4d719
zxcursed2008 <158029793+zxcursed2008@users.noreply.github.com>
2024-02-13 20:03:27 +0200

    додано модельку для героя гри

--- game.js
+++ game.js
@@ -48,7 +48,6 @@ function create ()
 
         //створення платформ
         platforms.create(400, 568, 'ground').setScale(2).refreshBody();
-
         platforms.create(600, 400, 'ground');
         platforms.create(50, 250, 'ground');
         platforms.create(750, 220, 'ground');
