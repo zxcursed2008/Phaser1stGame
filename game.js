21457e61758272fc5144aa1b818c80f471bb8b03
zxcursed2008 <158029793+zxcursed2008@users.noreply.github.com>
2024-02-13 18:43:36 +0200

    Додано появу нової бомбочки при зробі кожної зірки

--- game.js
+++ game.js
@@ -151,16 +151,17 @@ function collectStar(player, star) {
     star.disableBody(true, true);
     score += 10;
     scoreText.setText('Score: ' + score);
+
+    var x =(player.x < 400) ? Phaser.Math.Between(400, 800) : Phaser.Math.Between(0, 400);
+    var bomb = bombs.create(x, 16, 'bomb');
+    bomb.setBounce(1);
+    bomb.setCollideWorldBounds(true);
+    bomb. setVelocity(Phaser.Math.Between(-200, 200),20);
+
     if (stars.countActive(true) === 0) {
         stars.children.iterate(function (child) {
             child.enableBody(true, child.x, 0, true, true);
         });
-        var x = (player.x < 400) ? Phaser.Math.Between(400, 800) : Phaser.Math.Between(0, 400);
-        var bomb = bombs.create(x, 16, 'bomb');
-        bomb.setBounce(1);
-        bomb.setCollideWorldBounds(true);
-        bomb.setVelocity(Phaser.Math.Between(-200, 200), 20);
-        bomb.allowGravity = false;
     }
 }
 
